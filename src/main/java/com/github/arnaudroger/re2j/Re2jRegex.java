7c002c618d3be002d86350983d1619e7b4cc5b22
Arnaud Roger <arnaud.roger@gmail.com>
2018-03-22 21:42:35 +0000

    fix build

--- src/main/java/com/github/arnaudroger/re2j/Re2jRegex.java
+++ src/main/java/com/github/arnaudroger/re2j/Re2jRegex.java
@@ -62,14 +62,14 @@ public class Re2jRegex {
     @Benchmark
     public void testExp1(Blackhole blackhole) {
         for(String str : data) {
-            blackhole.consume(exp1.find(str));
+            blackhole.consume(exp1.matcher(str).find());
         }
     }
     
     @Benchmark
     public void testExp2(Blackhole blackhole) {
         for(String str : data) {
-            blackhole.consume(exp2.find(str));
+            blackhole.consume(exp2.matcher(str).find());
         }
     }
 }
